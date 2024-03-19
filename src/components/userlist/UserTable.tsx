3ca35d00871e4e1a59a5ec72a8385a8cdb6b0a5e
John <unitejohndalev@gmail.com>
2024-03-20 06:02:18 +0800

    User Table UI

--- src/components/userlist/UserTable.tsx
+++ src/components/userlist/UserTable.tsx
@@ -9,6 +9,11 @@ import { IoKey, IoFilterSharp } from "react-icons/io5";
 import { Link } from "react-router-dom";
 import { TfiLayoutColumn3Alt } from "react-icons/tfi";
 import { CgExport } from "react-icons/cg";
+import { MdArrowDropUp, MdArrowDropDown } from "react-icons/md";
+import { IoPersonAdd } from "react-icons/io5";
+import { SlOptionsVertical } from "react-icons/sl";
+import { GrMoreVertical } from "react-icons/gr";
+import { LuMinimize2, LuMaximize2 } from "react-icons/lu";
 
 type userData = {
   id: number;
@@ -26,6 +31,11 @@ const UserTable = () => {
   const dispatch = useDispatch();
 
   const [showTopBar, setShowTopBar] = useState(true);
+  const [searchOptions, setSearchOptions] = useState(false);
+  const [deleteHover, setDeleteHover] = useState(false);
+  const [addUserHover, setAddUserHover] = useState(false);
+  const [exportTable, setExportTable] = useState(false);
+  const [showMore, setShowMore] = useState(false);
 
   useEffect(() => {
     dispatch(getUserListFetch());
@@ -36,11 +46,65 @@ const UserTable = () => {
   );
   return (
     <table className="w-[100%] border-[.1rem] rounded-md shadow-sm bg-white  gap-x-5 flex flex-col items-center pb-2">
-      <div className="flex justify-center w-full text-[#24288A] text-[1.5rem]  cursor-pointer transition-all ease-linear">
-        {showTopBar ? (
-          <BsArrowBarDown onClick={() => setShowTopBar((prev) => !prev)} />
-        ) : (
-          <BsArrowBarUp onClick={() => setShowTopBar((prev) => !prev)} />
+      <div className="flex justify-between  text-[#24288A] text-[1.5rem] w-full cursor-pointer transition-all ease-linear relative">
+        <button
+          className="relative flex items-end px-2 py-1 font-bold"
+          onMouseEnter={() => setAddUserHover(true)}
+          onMouseLeave={() => setAddUserHover(false)}>
+          <span>
+            <IoPersonAdd className="text-[1.5rem] text-[#24288A]" />
+          </span>
+          {addUserHover && (
+            <div className="absolute top-0 w-[5rem] left-[1.2rem] font-bold text-[.7rem]">
+              ADD USER
+            </div>
+          )}
+        </button>
+        <span
+          onClick={() => setShowMore((prev) => !prev)}
+          className="items-end py-1">
+          <GrMoreVertical />
+        </span>
+        {showMore && (
+          <div className="absolute w-[10rem]  flex flex-col top-[1.9rem] px-2 py-1 right-[.5rem] text-[1rem] border-[.1rem] rounded-md shadow-sm bg-white">
+            {showTopBar ? (
+              <span
+                onClick={() => {
+                  setShowTopBar((prev) => !prev);
+                  setShowMore(false);
+                }}
+                className="hover:bg-[#C9E7F9] py-[.1rem] px-[.15rem] rounded-md text-[.8rem] cursor-pointer flex items-center justify-between">
+                Minimize
+                <LuMinimize2 />
+              </span>
+            ) : (
+              <span
+                onClick={() => {
+                  setShowTopBar((prev) => !prev);
+                  setShowMore(false);
+                }}
+                className="hover:bg-[#C9E7F9] py-[.1rem] px-[.15rem] rounded-md text-[.8rem] cursor-pointer flex items-center justify-between">
+                Expand
+                <LuMaximize2 />
+              </span>
+            )}
+            <span className="hover:bg-[#C9E7F9] py-[.1rem] px-[.15rem] rounded-md text-[.8rem] cursor-pointer flex items-center justify-between">
+              Export
+              <CgExport />
+            </span>
+            <span className="hover:bg-[#C9E7F9] py-[.1rem] px-[.15rem] rounded-md text-[.8rem] cursor-pointer flex items-center justify-between">
+              Columns
+              <TfiLayoutColumn3Alt />
+            </span>
+            <span className="hover:bg-[#C9E7F9] py-[.1rem] px-[.15rem] rounded-md text-[.8rem] cursor-pointer flex items-center justify-between">
+              Filters
+              <IoFilterSharp />
+            </span>
+            <span className="hover:bg-[#C9E7F9] py-[.1rem] px-[.15rem] rounded-md text-[.8rem] cursor-pointer flex items-center justify-between">
+              Density
+              <MdTableRows />
+            </span>
+          </div>
         )}
       </div>
       <div
@@ -50,63 +114,85 @@ const UserTable = () => {
             : "h-[0rem] transition-all ease-linear duration-100"
         }>
         {showTopBar && (
-          <>
-            <form className="flex justify-between ">
-              <input
-                type="text"
-                className="border-[.1rem] w-[20%]  py-2 px-2 rounded-md text-[.9rem] font-medium outline-none"
-                placeholder="Name"
-              />
-              <input
-                type="text"
-                className="border-[.1rem] w-[20%] py-2 px-2 rounded-md text-[.9rem] font-medium outline-none"
-                placeholder="Position"
-              />
-              <input
-                type="text"
-                className="border-[.1rem]  w-[20%] py-2 px-2 rounded-md text-[.9rem] font-medium outline-none"
-                placeholder="Business Unit"
-              />
-              <input
-                type="text"
-                className="border-[.1rem] w-[20%]  py-2 px-2 rounded-md text-[.9rem] font-medium outline-none"
-                placeholder="Department"
-              />
-              <button className="flex px-4 py-1 font-medium rounded-md gap-x-2 bg-[#E5E7EB] justify-center items-center">
-                <MdSearch className="text-[1.5rem]" />
-                Search
-              </button>
-            </form>
-            <div className="flex">
-              <button>DELETE</button>
-              <button>SEARCH</button>
-              <button>ADD USER</button>
-              <button className="flex">
+          <div className="flex items-end justify-between h-[2.5rem]">
+            {/* <div className="w-[5%] flex justify-center ">
+              <button
+                className="relative flex items-end font-bold transition-all ease-linear"
+                onMouseEnter={() => setDeleteHover(true)}
+                onMouseLeave={() => setDeleteHover(false)}>
                 <span>
-                  <TfiLayoutColumn3Alt />
+                  <MdDeleteOutline className="text-[1.5rem] text-red-500" />
                 </span>
-                <span>COLUMNS</span>
+                {deleteHover && (
+                  <div className="absolute top-[-.8rem] left-[1rem] font-bold text-[.7rem]">
+                    DELETE
+                  </div>
+                )}
               </button>
-              <button className="flex">
-                <span>
-                  <IoFilterSharp />
-                </span>
-                <span>FILTERS</span>
-              </button>
-              <button className="flex">
-                <span>
-                  <MdTableRows />
-                </span>
-                <span>DENSITY</span>
-              </button>
-              <button className="flex">
-                <span>
-                  <CgExport />
-                </span>
-                <span>EXPORT</span>
-              </button>
+            </div> */}
+            <div className="w-[5%]"></div>
+            <div className="flex items-center justify-center h-full ">
+              <form
+                className="flex w-[18rem] justify-evenly gap-x-2 h-full
+               items-center relative border-[.1rem] rounded-md shadow-sm px-2">
+                <button className="items-center justify-center font-medium ">
+                  <MdSearch className="text-[1.5rem]" />
+                </button>
+                <div className="flex items-center justify-center ">
+                  <input
+                    type="text"
+                    className="outline-none placeholder:pl-2"
+                    placeholder="Search User"
+                  />
+                </div>
+                <div className=" text-[1.5rem] cursor-pointer flex items-center justify-center">
+                  {searchOptions ? (
+                    <MdArrowDropUp
+                      onClick={() => setSearchOptions((prev) => !prev)}
+                    />
+                  ) : (
+                    <MdArrowDropDown
+                      onClick={() => setSearchOptions((prev) => !prev)}
+                    />
+                  )}
+                </div>
+                {searchOptions && (
+                  <div className="absolute w-[10rem] top-[2.7rem] right-0 text-[1rem] border-[.1rem] rounded-md shadow-sm bg-white">
+                    <div className="w-full px-2 py-1">
+                      <p className="hover:bg-[#C9E7F9] py-[.1rem] px-[.15rem] rounded-md text-[.8rem] cursor-pointer">
+                        Name
+                      </p>
+                      <p className="hover:bg-[#C9E7F9] py-[.1rem] px-[.15rem] rounded-md text-[.8rem] cursor-pointer">
+                        Position
+                      </p>
+                      <p className="hover:bg-[#C9E7F9] py-[.1rem] px-[.15rem] rounded-md text-[.8rem] cursor-pointer">
+                        Business Unit
+                      </p>
+                      <p className="hover:bg-[#C9E7F9] py-[.1rem] px-[.15rem] rounded-md text-[.8rem] cursor-pointer">
+                        Department
+                      </p>
+                    </div>
+                  </div>
+                )}
+              </form>
             </div>
-          </>
+            {/* <div className="flex justify-center w-[5%]">
+              <button
+                className="relative flex items-end font-bold transition-all ease-linear"
+                onMouseEnter={() => setExportTable(true)}
+                onMouseLeave={() => setExportTable(false)}>
+                <span>
+                  <CgExport className="text-[1.5rem] text-[#24288A]" />
+                </span>
+                {exportTable && (
+                  <div className="absolute top-[-.8rem] left-[-2.5rem] font-bold text-[.7rem]">
+                    EXPORT
+                  </div>
+                )}
+              </button>
+            </div> */}
+            <div className="w-[5%]"></div>
+          </div>
         )}
       </div>
       <div className="w-[98%] border-[.1rem] rounded-md shadow-sm bg-white h-[100vh]">
