256679bacafa4f1a761db201b5642d8f81ac3345
John <unitejohndalev@gmail.com>
2024-03-20 13:37:27 +0800

    create redux state for side bar

--- src/components/userlist/userListChildren/UserListTableData.tsx
+++ src/components/userlist/userListChildren/UserListTableData.tsx
@@ -7,6 +7,8 @@ import { MdDeleteOutline } from "react-icons/md";
 import { IoKey } from "react-icons/io5";
 import { userData } from "../userListTypes";
 
+//import module style css
+import UserListTableDataStyle from "../UserList.module.css";
 const UserListTableData = () => {
   const dispatch = useDispatch();
 
@@ -19,27 +21,37 @@ const UserListTableData = () => {
   const userList = useSelector(
     (state: RootState) => state.userListReducer.userList
   );
+  //raw data for table head
+  const thData = [
+    "Username",
+    "First Name",
+    "Last Name",
+    "Position",
+    "Email",
+    "Business Unit",
+    "Department",
+    "Created",
+    "Actions",
+  ];
 
   return (
-    <div className="w-[98%] border-[.1rem] rounded-md shadow-sm bg-white h-[100vh]">
-      <tr className="flex w-full  border-b-[.1rem] py-1 gap-x-5 shadow-sm font-medium text-[.8rem] ">
-        <th className="w-[5%] flex items-center justify-center">
+    <div className={UserListTableDataStyle.container}>
+      <tr className={UserListTableDataStyle.trContainer}>
+        <th className={UserListTableDataStyle.thInputContainer}>
           <input
             type="checkbox"
             name=""
             id=""
-            className="h-[1rem] cursor-pointer"
+            className={UserListTableDataStyle.thInput}
           />
         </th>
-        <th className="w-[10%]">Username</th>
-        <th className="w-[10%]">First Name</th>
-        <th className="w-[10%]">Last Name</th>
-        <th className="w-[10%]">Position</th>
-        <th className="w-[10%]">Email</th>
-        <th className="w-[10%] line-clamp-1">Business Unit</th>
-        <th className="w-[10%]">Department</th>
-        <th className="w-[10%]">Created</th>
-        <th className="w-[10%]">Actions</th>
+        {thData.map((thD, idx) => {
+          return (
+            <th key={idx} className={UserListTableDataStyle.thData}>
+              {thD}
+            </th>
+          );
+        })}
       </tr>
       {userList.map((user: userData, idx) => {
         const {
@@ -52,41 +64,38 @@ const UserListTableData = () => {
           section_name,
           reg_date,
         } = user;
-        console.log(user);
         return (
-          <tr
-            key={idx}
-            className="flex w-full border-b-[.1rem]  gap-x-5 py-1  shadow-sm text-[.8rem] font-medium">
-            <td className="w-[5%] flex items-center justify-center">
+          <tr key={idx} className={UserListTableDataStyle.trDataContainer}>
+            <td className={UserListTableDataStyle.thInputContainer}>
               <input
                 type="checkbox"
                 name=""
                 id=""
-                className="h-[1rem] cursor-pointer"
+                className={UserListTableDataStyle.thInput}
               />
             </td>
-            <td className="w-[10%] line-clamp-1 text-center">
+            <td className={UserListTableDataStyle.tdContainer}>
               <Link to="" className="text-blue-700 underline">
                 {username}
               </Link>
             </td>
-            <td className="w-[10%] line-clamp-1 text-center">{fname}</td>
-            <td className="w-[10%] line-clamp-1 text-center">{lname}</td>
-            <td className="w-[10%] line-clamp-1 text-center">
+            <td className={UserListTableDataStyle.tdContainer}>{fname}</td>
+            <td className={UserListTableDataStyle.tdContainer}>{lname}</td>
+            <td className={UserListTableDataStyle.tdContainer}>
               {position_sh_name}
             </td>
-            <td className="w-[10%] whitespace-nowrap overflow-hidden overflow-ellipsis text-center">
-              {email}
+            <td className={UserListTableDataStyle.tdEmail}>{email}</td>
+            <td className={UserListTableDataStyle.tdContainer}>{dept_name}</td>
+            <td className={UserListTableDataStyle.tdContainer}>
+              {section_name}
             </td>
-            <td className="w-[10%] line-clamp-1 text-center">{dept_name}</td>
-            <td className="w-[10%] line-clamp-1 text-center">{section_name}</td>
-            <td className="w-[10%] line-clamp-1 text-center">{reg_date}</td>
-            <td className="w-[10%] flex justify-center items-center gap-x-2">
-              <span className="text-[1.3rem] cursor-pointer text-[#78797c]">
+            <td className={UserListTableDataStyle.tdContainer}>{reg_date}</td>
+            <td className={UserListTableDataStyle.tdDeleteIconContainer}>
+              <span className={UserListTableDataStyle.tdIcon}>
                 <MdDeleteOutline />
               </span>
-              <div className="h-[1rem] border-[.01rem] border-[#E5E7EB]"></div>
-              <span className="text-[1.3rem] cursor-pointer text-[#78797c]">
+              <div className={UserListTableDataStyle.lineDiv}></div>
+              <span className={UserListTableDataStyle.tdIcon}>
                 <IoKey />
               </span>
             </td>
---
+++ src/redux/state/sidebarState.ts
@@ -0,0 +1,162 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+
+//sideBarTExt state
+const sideBarTextSlice = createSlice({
+    name: "sideBarText",
+    initialState: {
+        sideBarTextValue: true
+    },
+    reducers: {
+        setShowSideBarText: (state) => {
+            state.sideBarTextValue = !state.sideBarTextValue
+        },
+        setShowSideBarTextFalse: (state) => {
+            state.sideBarTextValue = false
+        }
+    }
+})
+
+export const { setShowSideBarText, setShowSideBarTextFalse } = sideBarTextSlice.actions
+export const sideBarTextReducer = sideBarTextSlice.reducer
+
+
+//otherSideBarText1 state
+const otherSideBarTextSlice1 = createSlice({
+    name: "otherSideBarText1",
+    initialState: {
+        otherSideBarTextValue: false
+    },
+    reducers: {
+        setShowOtherSideBarText1: (state) => {
+            state.otherSideBarTextValue =!state.otherSideBarTextValue
+        },
+        setShowOtherSideBarText1False: (state) => {
+            state.otherSideBarTextValue = false
+        }
+    }
+})
+
+export const { setShowOtherSideBarText1, setShowOtherSideBarText1False } = otherSideBarTextSlice1.actions
+export const otherSideBarText1Reducer = otherSideBarTextSlice1.reducer
+
+//othersSideBarText2 state
+
+const othersSideBarTextSlice2 = createSlice({
+    name: "othersSideBarText2",
+    initialState: {
+        othersSideBarTextValue: false
+    },
+    reducers: {
+        setShowOthersSideBarText2: (state) => {
+            state.othersSideBarTextValue =!state.othersSideBarTextValue
+        },
+        setShowOthersSideBarText2False: (state) => {
+            state.othersSideBarTextValue = false
+        }
+    }
+})
+
+export const { setShowOthersSideBarText2, setShowOthersSideBarText2False } = othersSideBarTextSlice2.actions
+export const otherSideBartText2Reducer = othersSideBarTextSlice2.reducer;
+
+//hoverDashBoardText state
+
+const hoverDashBoardTextSlice = createSlice({
+    name: "hoverDashBoardText",
+    initialState: {
+        hoverDashBoardTextValue: false
+    },
+    reducers: {
+        setShowHoverDashBoardText: (state) => {
+            state.hoverDashBoardTextValue =!state.hoverDashBoardTextValue
+        },
+        setShowHoverDashBoardTextFalse: (state) => {
+            state.hoverDashBoardTextValue = false
+        }
+    }
+})
+
+export const { setShowHoverDashBoardText, setShowHoverDashBoardTextFalse } = hoverDashBoardTextSlice.actions
+export const hoverDashBoardTextReducer = hoverDashBoardTextSlice.reducer;
+
+//hoverUserText state
+
+const hoverUserTextSlice = createSlice({
+    name: "hoverUserText",
+    initialState: {
+        hoverUserTextValue: false
+    },
+    reducers: {
+        setShowHoverUserText: (state) => {
+            state.hoverUserTextValue =!state.hoverUserTextValue
+        },
+        setShowHoverUserTextFalse: (state) => {
+            state.hoverUserTextValue = false
+        }
+    }
+})
+
+export const { setShowHoverUserText, setShowHoverUserTextFalse } = hoverUserTextSlice.actions
+export const hoverUserTextReducer = hoverUserTextSlice.reducer
+
+//hoverProjectText state
+
+const hoverProjectTextSlice = createSlice({
+    name: "hoverProjectText",
+    initialState: {
+        hoverProjectTextValue: false
+    },
+    reducers: {
+        setShowHoverProjectText: (state) => {
+            state.hoverProjectTextValue =!state.hoverProjectTextValue
+        },
+        setShowHoverProjectTextFalse: (state) => {
+            state.hoverProjectTextValue = false
+        }
+    }
+})
+
+export const { setShowHoverProjectText, setShowHoverProjectTextFalse } = hoverProjectTextSlice.actions
+export const hoverProjectTextReducer = hoverProjectTextSlice.reducer
+
+
+//hoverRoleText state
+
+const hoverRoleTextSlice = createSlice({
+    name: "hoverRoleText",
+    initialState: {
+        hoverRoleTextValue: false
+    },
+    reducers: {
+        setShowHoverRoleText: (state) => {
+            state.hoverRoleTextValue =!state.hoverRoleTextValue
+        },
+        setShowHoverRoleTextFalse: (state) => {
+            state.hoverRoleTextValue = false
+        }
+    }
+})
+
+export const { setShowHoverRoleText, setShowHoverRoleTextFalse } = hoverRoleTextSlice.actions
+export const hoverRoleTextReducer = hoverRoleTextSlice.reducer
+
+//hoverOthersText state
+
+const hoverOthersTextSlice = createSlice({
+    name: "hoverOthersText",
+    initialState: {
+        hoverOthersTextValue: false
+    },
+    reducers: {
+        setShowHoverOthersText: (state) => {
+            state.hoverOthersTextValue =!state.hoverOthersTextValue
+        },
+        setShowHoverOthersTextFalse: (state) => {
+            state.hoverOthersTextValue = false
+        }
+    }
+})
+
+export const { setShowHoverOthersText, setShowHoverOthersTextFalse } = hoverOthersTextSlice.actions
+export const hoverOthersTextReducer = hoverOthersTextSlice.reducer
